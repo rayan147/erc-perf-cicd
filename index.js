0ce6f1788c0b27448070e6e0ee9f74520d20bce6
Rayan Ramirez <rayan361@gmail.com>
2022-03-14 10:24:12 -0400

    changed AWS_DEFAULT_RGION to AWS_REGION  circleci

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config({path:"./env.local"});
 
  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
- const region = process.env.AWS_DEFAULT_REGION;
+ const region = process.env.AWS_REGION;
 
 const s3 = new AWS.S3({
     accessKeyId,
