915f574c591e5ac15b81a539bf322c020ba12083
Rayan Ramirez <rayan361@gmail.com>
2022-03-14 15:00:59 -0400

    added private erc repo

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,19 @@
 # stage 1 buiding image
-FROM  node:16.0.0 as builder
+FROM  node:17.0.0 as builder
 WORKDIR /app
 ENV NODE_ENV=production
+ENV  S3_URL=${S3_URL}
 COPY package*.json ./
 RUN npm install
 COPY . .
-RUN npm run build
+
 
 # stage 2 run image
-FROM  node:16.0.0 as runner
+FROM  node:17.0.0 
 WORKDIR /app
 COPY package*.json ./
 RUN npm ci --only=production
-COPY --from=builder /app/dist ./dist
-CMD node ./dist/index.js 
+COPY --from=builder ./app /app/
+CMD ["sh", "-c", "node index.js ${S3_URL}"] 
 
 
--- index.js
+++ index.js
@@ -1,42 +1,35 @@
-import * as readline from 'node:readline/promises';
-import dotenv from 'dotenv';
-import AWS from 'aws-sdk';
-import fs from 'node:fs';
-import path from 'node:path';
+import dotenv from "dotenv";
+import AWS from "aws-sdk";
+import fs from "node:fs";
+import path from "node:path";
 
-import { stdin as input, stdout as output } from 'node:process';
-import { fileURLToPath } from 'node:url';
+import { stdin as input, stdout as output } from "node:process";
+import { fileURLToPath } from "node:url";
 
-
+dotenv.config({ path: "./env.local" });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-dotenv.config({path:"./env.local"});
-
- const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
- const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
- const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region = process.env.AWS_REGION;
 
 const s3 = new AWS.S3({
-    accessKeyId,
-    secretAccessKey,
-    region,
+  accessKeyId,
+  secretAccessKey,
+  region,
 });
 
-// ask the url to provide a valid s3 url
-const readlineInterface = readline.createInterface({ input, output });
-const getUrlFromUser = await readlineInterface.question('Enter the s3 url: ');
+// get the bucket name from the url
+const getBucketNameAndFileName = (url) => {
+  const urlParts = url.split("/");
+  const bucketName = urlParts[2];
+  const fileName = urlParts.slice(3).join("/");
+  return { bucketName, fileName };
+};
 
-// is the url valid?
-const s3Regex = /^s3:\/\/([^\/]+)\/(.+)$/;
-const url = getUrlFromUser.match(s3Regex);
-if (!url) {
-    throw new Error('Invalid s3 url');
-}
 
-const bucketName = url[1];
-const fileName = url[2];
-
+const { bucketName, fileName } = getBucketNameAndFileName(process.env.S3_URL);
+console.log(`Downloading ${fileName} from ${bucketName}`);
 // get the file from s3
 const getFileFromStream = (fileName) => {
   const params = {
@@ -44,19 +37,15 @@ const getFileFromStream = (fileName) => {
     Key: fileName,
   };
   return s3.getObject(params).createReadStream();
-}
-console.log('Downloading file...');
-const s3FileStream =  getFileFromStream(fileName).pipe(fs.createWriteStream(__dirname + '/' + fileName));
+};
+console.log("Downloading file...");
+const s3FileStream = getFileFromStream(fileName).pipe(
+  fs.createWriteStream(__dirname + "/" + fileName)
+);
 // get downloaded file and pipe it to stdout
-s3FileStream.on('finish', () => {
-    fs.createReadStream(__dirname + '/' + fileName).pipe(output);
-    });
+s3FileStream.on("finish", () => {
+  fs.createReadStream(__dirname + "/" + fileName).pipe(output);
+});
 
-console.log('File downloaded');
-console.log('File is being piped to stdout');
-readlineInterface.close();
-
-
-
-
-
+console.log("File downloaded");
+console.log("File is being piped to stdout");
